8da5492e8daf7f7386ebafbf3be1a87e56eb67c0
Daniel Chen <72116969+dannychen0830@users.noreply.github.com>
2021-11-06 15:24:10 -0400

    Add files via upload

--- gen_sensor_measurement.m
+++ gen_sensor_measurement.m
@@ -2,22 +2,22 @@
 % measurement
 % input: 
 %   x - true distance from robot to obstable in the direction of bearing
+%   param - parameters for error [a, b, max, s, w]
+%            a, b - linear function, max - max reading, s - std, w - error
+%            to gaussian weight
 % output: 
 %   y - a measurement 
-function y = gen_sensor_measurement(x)
 
-max = 5; % maximum sensor reading
+% param = [0.05, 0.2, 5, 0.1, 0.15]
+function y = gen_sensor_measurement(x, param)
+a = param(1); b = param(2); max = param(3); s = param(4); w = param(5); 
 
 % generate model for the case where distance exceeds max
-a = 0.05; b = 0.2; 
 xi = rand; 
 if xi < a, z = 0; 
 elseif a < xi && xi < b, z = (xi - a)*max/(b-a); 
 else, z = max; 
 end
-
-s = 0.1; % variance of gaussian 
-w = 0.1; % prob. of wrong reading vs. gaussian reading
 
 % if true distance exceed max reading distance, draw from error
 % distribution
